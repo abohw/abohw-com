9096144e7ec9dabba3202bf7472d2c512fb54c6f
Alex Bowman <alex@abxmb.com>
2021-01-15 13:09:27 -0600

    fixing foursquare command

--- web/management/commands/cache_fsq.py
+++ web/management/commands/cache_fsq.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             '--initial',
-            action='bulk_fill',
+            action='store_true',
             help='fill initial foursquare cache from nothing',
         )
 
