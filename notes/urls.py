b502cf695095d46b8f9140fe5556cfc9c04f1d13
Alex Bowman <alex@abxmb.com>
2021-07-07 15:06:06 -0500

    extended rss feed

--- notes/urls.py
+++ notes/urls.py
@@ -18,11 +18,11 @@ from django.urls import re_path, include
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.contrib.sitemaps.views import sitemap
-from wagtail_feeds.feeds import BasicFeed
+from wagtail_feeds.feeds import ExtendedFeed
 
 urlpatterns = [
     re_path('sitemap.xml', sitemap),
     re_path(r'^cms/', include(wagtailadmin_urls)),
     re_path(r'', include(wagtail_urls)),
-    re_path(r'^feed$', BasicFeed(), name='basic_feed'),
+    re_path(r'^feed$', ExtendedFeed(), name='extended_feed'),
 ]
