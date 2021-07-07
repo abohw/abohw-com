bbc8ff069dc5236ae7aa3a77f8c416e1bb7baf28
Alex Bowman <alex@abxmb.com>
2021-07-07 14:57:02 -0500

    add rss

--- notes/urls.py
+++ notes/urls.py
@@ -17,7 +17,6 @@ from django.urls import re_path, include
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.contrib.sitemaps.views import sitemap
 from wagtail_feeds.feeds import BasicFeed
 
