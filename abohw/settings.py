d02030931dd3177dfce44a67268e10f68b204278
Alex Bowman <alex@abxmb.com>
2021-10-07 16:27:18 -0500

    oops, don't update settings.py

--- abohw/settings.py
+++ abohw/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-if DEBUG is True:
-    load_dotenv()
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -73,6 +69,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'photos',
+    'work',
     'notes',
     'web',
 ]
